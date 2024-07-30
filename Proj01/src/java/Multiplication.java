fbdd6e9d9cd06312de67959a649ed404c4cd8996
SMART <SMART@DESKTOP-TTCTVFD>
2024-07-30 21:49:54 +0530

    commit and Push

---
+++ Proj01/src/java/Multiplication.java
@@ -0,0 +1,12 @@
+package java;
+
+public class Multiplication {
+	public static int mul(int a, int b) {
+		return a*b;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		mul(10,20);
+	}
+
+}
