8926b0a81c0b943d5e3f156a3c00c269bb80fad7
SMART <SMART@DESKTOP-TTCTVFD>
2024-07-30 21:12:42 +0530

    commit and Push

--- Proj01/src/java/Addition.java
+++ Proj01/src/java/Addition.java
@@ -1,13 +1,13 @@
 package java;
 
 public class Addition {
-	public static int Add(int a, int b) {
-		return a+b;
+	public static int Add(int a, int b, int c) {
+		return a+b+c;
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        Add(10,20);
+        Add(10,20,30);
 	}
 
 }
