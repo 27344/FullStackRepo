2bf7f9b1910e8b4f753bb14dc24af2f5c7752782
SMART <SMART@DESKTOP-TTCTVFD>
2024-07-30 19:06:11 +0530

    commit and Push

---
+++ Proj01/src/java/Subtraction.java
@@ -0,0 +1,14 @@
+package java;
+
+public class Subtraction {
+
+	public static int Sub(int a, int b) {
+		return a-b;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Sub(10, 20);
+
+	}
+
+}
